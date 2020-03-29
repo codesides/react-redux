d3d5172f95672107a0586011cb7a029e5bb5efe4
1179850608@qq.com <1179850608@qq.com>
2020-03-29 20:02:14 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+/*
+ * @Author: codesides
+ * @Descripttion:
+ * @Date: 2020-03-29 19:58:48
+ * @LastEditors: codesides
+ * @LastEditTime: 2020-03-29 19:59:48
+ * @FilePath: /react_redux/src/App.jsx
+ */
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return <div>App</div>;
+  }
+}
+
+export default App;
